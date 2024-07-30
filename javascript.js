7fc387a9092eae61c232af2db7ae00275e455be2
bjornrohrer <barohrerdk@gmail.com>
2024-07-30 18:18:36 +0200

    Added html boilerplate and played around with userChoice and created some if and else if statements to get the program to run.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let userChoice = prompt("Rock, Paper or Scissors", "")[
+  ("rock", "paper", "scissors")
+];
+
+if (userChoice === "rock" && computerChoice === "scissors") {
+  alert("You won");
+} else if (userChoice === "paper" && computerChoice === "rock") {
+  alert("You won");
+} else if (userChoice === "scissors" && computerChoice === "paper") {
+  alert("You won");
+}
