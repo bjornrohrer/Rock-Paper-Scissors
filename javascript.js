b9bdce55f2e6a24e6583343d3e4d22bb420b4a2e
bjornrohrer <barohrerdk@gmail.com>
2024-08-07 16:35:43 +0200

    created a function called 'gameLogic'

--- javascript.js
+++ javascript.js
@@ -4,10 +4,12 @@ let userChoice = prompt("Rock, Paper or Scissors", "")[
   ("rock", "paper", "scissors")
 ];
 
-if (userChoice === "rock" && computerChoice === "scissors") {
-  alert("You won");
-} else if (userChoice === "paper" && computerChoice === "rock") {
-  alert("You won");
-} else if (userChoice === "scissors" && computerChoice === "paper") {
-  alert("You won");
+function gameLogic() {
+  if (userChoice === "rock" && computerChoice === "scissors") {
+    alert("You won");
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    alert("You won");
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    alert("You won");
+  }
 }
