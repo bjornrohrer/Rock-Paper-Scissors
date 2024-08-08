9a772a49942ef18033669b4cab5edab3fbddd2b1
bjornrohrer <barohrerdk@gmail.com>
2024-08-08 14:16:10 +0200

    Removed scoretracker function. Added the code inside the gamelogic function

--- javascript.js
+++ javascript.js
@@ -12,37 +12,29 @@ const computerChoice = generateComputerChoice();
 function gameLogic() {
   const computerChoice = generateComputerChoice();
 
-  if (userChoice === "rock" && computerChoice === "scissors") {
-    alert("You won");
-  } else if (userChoice === "paper" && computerChoice === "rock") {
-    alert("You won");
-  } else if (userChoice === "scissors" && computerChoice === "paper") {
-    alert("You won");
-  } else if (userChoice === "scissors" && computerChoice === "rock") {
-    alert("You lost");
-  } else if (userChoice === "rock" && computerChoice === "paper") {
-    alert("You lost");
-  } else if (userChoice === "paper" && computerChoice === "scissors") {
-    alert("You lost");
-  } else {
-    alert("draw");
-  }
-
-  scoreTracker();
-}
-
-function scoreTracker() {
   let userScore = 0;
   let computerScore = 0;
 
-  if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-  ) {
+  if (userChoice === "rock" && computerChoice === "scissors") {
+    alert("You won");
     userScore++;
-  } else {
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    alert("You won");
+    userScore++;
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    alert("You won");
+    userScore++;
+  } else if (userChoice === "scissors" && computerChoice === "rock") {
+    alert("You lost");
     computerScore++;
+  } else if (userChoice === "rock" && computerChoice === "paper") {
+    alert("You lost");
+    computerScore++;
+  } else if (userChoice === "paper" && computerChoice === "scissors") {
+    alert("You lost");
+    computerScore++;
+  } else {
+    alert("draw");
   }
 
   console.log(`Score :You ${userScore} - ${computerScore} Computer`);
