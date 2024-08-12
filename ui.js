dd2db11074c3a4e3044b123bda90f3b164068abb
bjornrohrer <barohrerdk@gmail.com>
2024-08-12 19:38:24 +0200

    Changed the code completly

--- ui.js
+++ ui.js
@@ -1,45 +1,19 @@
-const computerOptions = ["rock", "paper", "scissors"];
+function playGame(userChoice) {
+  const choices = ["rock", "paper", "scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
 
-function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * computerOptions.length);
-  return computerOptions[randomNumber];
-}
-
-function userChoice() {
-  const buttonRock = document.querySelector(".rock");
-  const buttonPaper = document.querySelector(".paper");
-  const buttonScissors = document.querySelector(".scissors");
-}
-
-function gameLogic() {
-  const userChoice = prompt("Rock, Paper or Scissors", "").toLowerCase();
-
-  const computerChoice = generateComputerChoice();
-
-  let userScore = 0;
-  let computerScore = 0;
-
-  if (userChoice === "rock" && computerChoice === "scissors") {
-    alert("You won");
-    userScore++;
-  } else if (userChoice === "paper" && computerChoice === "rock") {
-    alert("You won");
-    userScore++;
-  } else if (userChoice === "scissors" && computerChoice === "paper") {
-    alert("You won");
-    userScore++;
-  } else if (userChoice === "scissors" && computerChoice === "rock") {
-    alert("You lost");
-    computerScore++;
-  } else if (userChoice === "rock" && computerChoice === "paper") {
-    alert("You lost");
-    computerScore++;
-  } else if (userChoice === "paper" && computerChoice === "scissors") {
-    alert("You lost");
-    computerScore++;
+  if (userChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    result = `You win! ${userChoice} beats ${computerChoice}.`;
   } else {
-    alert("draw");
+    result = `You lose! ${computerChoice} beats ${userChoice}.`;
   }
 
-  console.log(`Score :You ${userScore} - ${computerScore} Computer`);
+  document.getElementById("result").innerText = result;
 }
