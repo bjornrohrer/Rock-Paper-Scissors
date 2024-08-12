b3768a8be3642a350d2047c1137783e0e53d0e5e
bjornrohrer <barohrerdk@gmail.com>
2024-08-12 19:23:19 +0200

    Changed from Large letters to small letters

---
+++ ui.js
@@ -0,0 +1,43 @@
+const computerOptions = ["rock", "paper", "scissors"];
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * computerOptions.length);
+  return computerOptions[randomNumber];
+}
+
+const buttonRock = document.querySelector("Rock");
+
+function userChoice() {}
+
+function gameLogic() {
+  const userChoice = prompt("Rock, Paper or Scissors", "").toLowerCase();
+
+  const computerChoice = generateComputerChoice();
+
+  let userScore = 0;
+  let computerScore = 0;
+
+  if (userChoice === "rock" && computerChoice === "scissors") {
+    alert("You won");
+    userScore++;
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    alert("You won");
+    userScore++;
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    alert("You won");
+    userScore++;
+  } else if (userChoice === "scissors" && computerChoice === "rock") {
+    alert("You lost");
+    computerScore++;
+  } else if (userChoice === "rock" && computerChoice === "paper") {
+    alert("You lost");
+    computerScore++;
+  } else if (userChoice === "paper" && computerChoice === "scissors") {
+    alert("You lost");
+    computerScore++;
+  } else {
+    alert("draw");
+  }
+
+  console.log(`Score :You ${userScore} - ${computerScore} Computer`);
+}
